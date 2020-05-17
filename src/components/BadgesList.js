9516e2d141b2dd68a86a9d6c35692709d76630b7
Brayan Llanos <brayan0428@gmail.com>
2020-05-17 11:54:46 -0500

    Implementacion de filtro de busqueda

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,27 +1,41 @@
-import React from 'react'
-import  './styles/BadgesList.css'
+import React, { useState } from 'react'
+import './styles/BadgesList.css'
 import { Link } from 'react-router-dom'
-class BadgesList extends React.Component{
-  render(){
-    return(
-      <div>
-        {
-          this.props.badges.map(badge => (
-            <Link className="text-decoration-none text-reset" to={`badges/${badge.id}`} key={badge.id}>
+
+function BadgesList(props) {
+
+  const [query, setQuery] = useState('')
+
+  const badges = props.badges
+
+  const filteredBadges = badges.filter(badge => {
+    return `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase())
+  })
+
+  return (
+    <div>
+      <div className="form-group">
+        <label>Buscar</label>
+        <input type="text" className="form-control" value={query} onChange={e => {
+          setQuery(e.target.value)
+        }}/>
+      </div>
+      {
+        filteredBadges.map(badge => (
+          <Link className="text-decoration-none text-reset" to={`badges/${badge.id}`} key={badge.id}>
             <div className="Badge__container">
-              <img src={badge.avatarUrl} alt="Avatar"/>
+              <img src={badge.avatarUrl} alt="Avatar" />
               <div className="Badge-info">
                 <h5 className="Badge-name">{badge.firstName} {badge.lastName}</h5>
                 <h6 className="Badge-twitter">@{badge.twitter}</h6>
                 <h6>{badge.jobTitle}</h6>
               </div>
             </div>
-            </Link>
-          ))
-        }
-      </div>
-    )
-  }
+          </Link>
+        ))
+      }
+    </div>
+  )
 }
 
 export default BadgesList
